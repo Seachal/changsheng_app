fa15149ac52be728c53e3fc00c3acc17b559606f
WSH <864849414@qq.com>
2022-03-24 15:30:50 +0800

    提交代码

--- lib/api/CSClassNetConfig.dart
+++ lib/api/CSClassNetConfig.dart
@@ -12,13 +12,13 @@ class CSClassNetConfig {
   static  IosDeviceInfo ?csProIosDeviceInfo;
 
   static var  BASE_URL_DEBUG = 'http://api-test.demo.gz583.com/';
-  static var  BASE_URL_RELEASE = 'http://csapi.gz583.cn/';
-  static var  BASE_URL_IOS_RELEASE = 'http://csapi.gz583.cn/';
+  static var  BASE_URL_RELEASE = 'http://api.win2048.com/';
+  static var  BASE_URL_IOS_RELEASE = 'http://api.win2048.com/';
   static var  SHARE_URL_DEBUG = 'http://demo.gz583.com/';
   static var  SHARE_URL_RELEASE = 'http://www.gz583.com/';
   static var  SHARE_URL_IOS_RELEASE = 'http://www.gz583.cn/';
   static var  IMAGE_URL_DEBUG = 'http://cdn.demo.gz583.com/';
-  static var  IMAGE_URL_RELEASE = 'http://cdn.gz583.com/';
+  static var  IMAGE_URL_RELEASE = 'http://cdn.win2048.com/';
 
   static csMethodGetBasicUrl() {
     return  CSClassApplicaion.csProDEBUG? CSClassNetConfig.BASE_URL_DEBUG:Platform.isIOS ? BASE_URL_IOS_RELEASE: CSClassNetConfig.BASE_URL_RELEASE;
--- lib/pages/CSClassSplashPage.dart
+++ lib/pages/CSClassSplashPage.dart
@@ -34,70 +34,90 @@ class CSClassSplashPageState extends State<CSClassSplashPage> {
       color: Colors.white,
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-                alignment: Alignment.center,
-                child: CachedNetworkImage(
-                  fit: BoxFit.cover,
-                  width: MediaQuery.of(context).size.width,
-                  imageUrl:
-                      "${CSClassNetConfig.csMethodGetImageUrl()}img/startup.png?time=${DateTime.now().millisecondsSinceEpoch.toString()}",
-                  imageBuilder: (context, imageProvider) => Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: imageProvider,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                  errorWidget: (context, url, error) =>
-                      Image.asset(
-                    CSClassImageUtil.csMethodGetImagePath("bg_splash"),
-                    fit: BoxFit.cover,
-                    width: MediaQuery.of(context).size.width,
-                  ),
-                )),
-          ),
-          Container(
-            height: width(80),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Image.asset(
-                      CSClassImageUtil.csMethodGetImagePath("cs_app_logo"),
-                      width: width(40),
-                    ),
-                    SizedBox(
-                      width: width(3),
-                    ),
-                    Text(
-                      "${CSClassApplicaion.csProAppName}",
-                      style: TextStyle(
-                          fontSize: sp(18), fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: height(5),
-                ),
-                Text(
-                  "Copyright © 2018-2020 " +
-                      "${CSClassApplicaion.csProAppName} " +
-                      "All Rights Reserved",
-                  style: TextStyle(fontSize: sp(9), color: Color(0xFF999999)),
-                )
-              ],
+      child: CachedNetworkImage(
+        fit: BoxFit.cover,
+        width: MediaQuery.of(context).size.width,
+        imageUrl:
+        "${CSClassNetConfig.csMethodGetImageUrl()}img/startup.png?time=${DateTime.now().millisecondsSinceEpoch.toString()}",
+        imageBuilder: (context, imageProvider) => Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: imageProvider,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
+          ),
+        ),
+        errorWidget: (context, url, error) =>
+            Image.asset(
+              CSClassImageUtil.csMethodGetImagePath("bg_splash"),
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width,
+            ),
       ),
+      // child: Column(
+      //   children: <Widget>[
+      //     Expanded(
+      //       child: Container(
+      //           alignment: Alignment.center,
+      //           child: CachedNetworkImage(
+      //             fit: BoxFit.cover,
+      //             width: MediaQuery.of(context).size.width,
+      //             imageUrl:
+      //                 "${CSClassNetConfig.csMethodGetImageUrl()}img/startup.png?time=${DateTime.now().millisecondsSinceEpoch.toString()}",
+      //             imageBuilder: (context, imageProvider) => Container(
+      //               decoration: BoxDecoration(
+      //                 image: DecorationImage(
+      //                   image: imageProvider,
+      //                   fit: BoxFit.cover,
+      //                 ),
+      //               ),
+      //             ),
+      //             errorWidget: (context, url, error) =>
+      //                 Image.asset(
+      //               CSClassImageUtil.csMethodGetImagePath("bg_splash"),
+      //               fit: BoxFit.cover,
+      //               width: MediaQuery.of(context).size.width,
+      //             ),
+      //           )),
+      //     ),
+      //     Container(
+      //       height: width(80),
+      //       child: Column(
+      //         crossAxisAlignment: CrossAxisAlignment.center,
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: <Widget>[
+      //           Row(
+      //             crossAxisAlignment: CrossAxisAlignment.center,
+      //             mainAxisAlignment: MainAxisAlignment.center,
+      //             children: <Widget>[
+      //               Image.asset(
+      //                 CSClassImageUtil.csMethodGetImagePath("cs_app_logo"),
+      //                 width: width(40),
+      //               ),
+      //               SizedBox(
+      //                 width: width(3),
+      //               ),
+      //               Text(
+      //                 "${CSClassApplicaion.csProAppName}",
+      //                 style: TextStyle(
+      //                     fontSize: sp(18), fontWeight: FontWeight.bold),
+      //               )
+      //             ],
+      //           ),
+      //           SizedBox(
+      //             height: height(5),
+      //           ),
+      //           Text(
+      //             "Copyright © 2018-2020 " +
+      //                 "${CSClassApplicaion.csProAppName} " +
+      //                 "All Rights Reserved",
+      //             style: TextStyle(fontSize: sp(9), color: Color(0xFF999999)),
+      //           )
+      //         ],
+      //       ),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
